065b30a6a6a0e5dc164d2eb7faf2a5c46338ce23
Magnus Nordlander <magnus@nordlander.se>
2018-03-06 15:57:16 +0100

    Updated dcomposer to pass memory limit env var.

--- functions/dcomposer.fish
+++ functions/dcomposer.fish
@@ -1,7 +1,7 @@
 function dcomposer
     if test -f .maincontainer
         set -lx COMPOSE_API_VERSION "auto"
-        docker-compose exec -e COMPOSER_AUTH=(jq -c . < ~/.composer/auth.json) (cat .maincontainer) composer $argv
+        docker-compose exec -e COMPOSER_AUTH=(jq -c . < ~/.composer/auth.json) -e COMPOSER_MEMORY_LIMIT=4G (cat .maincontainer) composer $argv
     else
         echo "Could not find .maincontainer file"
     end
