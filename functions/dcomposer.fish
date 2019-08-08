2aa1606c0f9c56d890c887fa87d4bf3dbc7cf949
Magnus Nordlander <magnus@nordlander.se>
2019-08-08 13:23:27 +0200

    Update dcomposer.fish

--- functions/dcomposer.fish
+++ functions/dcomposer.fish
@@ -1,7 +1,8 @@
 function dcomposer
     if test -f .maincontainer
         set -lx COMPOSE_API_VERSION "auto"
-        docker-compose exec -e COMPOSER_AUTH=(jq -c . < ~/.composer/auth.json) -e COMPOSER_MEMORY_LIMIT=4G (cat .maincontainer) composer $argv
+        set -lx __COMPOSER_AUTH (cat ~/.composer/auth.json)
+        docker-compose exec -e COMPOSER_AUTH="$__COMPOSER_AUTH" -e COMPOSER_MEMORY_LIMIT=4G (cat .maincontainer) composer $argv
     else
         echo "Could not find .maincontainer file"
     end
